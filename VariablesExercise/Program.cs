13b2038214ea85b305be6b1ecd4cfea488c5dc24
Shrividya Gopi <shrividya.gopi@gmail.com>
2024-10-07 20:25:10 -0400

    Variable Excercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Declaration and initializing the variables
+            String name = "Shrividya";
+            int age = 20;
+            char middlename = 'G';
+            bool isOver18 = true;
+            double currentTemp = 80.2;
+            decimal currentPrice = 10.55m;
+            
+            //Interpolate these variables in a Console.Writeline();
+            Console.WriteLine($"My name is : {name}");     
+            Console.WriteLine($"My age is : {age}"); 
+            Console.WriteLine($"My Middlename is : {middlename}");  
+            Console.WriteLine($"My Over18 is : {isOver18}"); 
+            Console.WriteLine($"My CurrentPrice is :{currentPrice}");
+            //Console.WriteLine("Hello, World!");
         }
     }
 }
